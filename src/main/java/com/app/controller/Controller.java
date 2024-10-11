90a4366842234c3a9cced23f353a9ded5bdf65df
danielpatricio0022 <hadukens@hotmail.com>
2024-10-11 10:25:27 -0300

    finally

--- src/main/java/com/app/controller/Controller.java
+++ src/main/java/com/app/controller/Controller.java
@@ -1,10 +1,10 @@
 package com.app.controller;
 
 import com.app.entity.Task;
-import com.app.repository.TaskRepositoryInterface;
 import com.app.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,10 +21,28 @@ public class Controller {
         return service.findAll();
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},
-                produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE}) //response formated
+    public Task findById(@PathVariable(value = "id") Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},//request
+                 produces = {MediaType.APPLICATION_JSON_VALUE}) //response formated
     public Task create(@RequestBody Task task){
     return service.createTask(task);
     }
+
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},//request
+                produces = {MediaType.APPLICATION_JSON_VALUE}) //response formated
+    public Task update(@RequestBody Task task){
+        return service.update(task);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/app/service/TaskService.java
+++ src/main/java/com/app/service/TaskService.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 
 @Service
 public class TaskService {
@@ -18,9 +18,19 @@ public class TaskService {
   TaskRepositoryInterface repository;
 
 public List<Task> findAll(){
+  logger.info("Finding all task!");
     List<Task> listEntity = repository.findAll();
-    logger.info("Finding all task!");
     return listEntity;
+}
+
+public Task findById(Long id){
+  var task = repository.findById(id);
+
+  if (task.isPresent()) {
+    return task.get();
+  } else {
+    throw new RuntimeException("Task not found with id: " + id);
+  }
 }
 
 public Task createTask(Task task){
@@ -28,12 +38,30 @@ public Task createTask(Task task){
     logger.error("error, task cant is null");
     throw new RuntimeException("task null");
   }
-  var entity = repository.save(task);
-  return entity;
+   repository.save(task);
+  return task;
 }
 
+  public Task update(Task task) {
+    if (task == null) throw new RuntimeException("Task cannot be null");
+    logger.info("Updating one task!");
 
+    Task entity = repository.findById(task.getId()).orElseThrow
+            (() -> new RuntimeException("not found id"));
+    entity.setTitle(task.getTitle());
+    entity.setDescription(task.getDescription());
+    entity.setStatus(task.getStatus());
 
+    return repository.save(entity);
+  }
+
+  public void delete(Long id){
+    logger.info("Deleting Task!");
+    var entity = repository.findById(id)
+            .orElseThrow(() -> new RuntimeException("No records found for this ID!"));
+    repository.delete(entity);
+
+  }
 
 
 
